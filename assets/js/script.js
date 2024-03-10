8a233b26b0457abf8c511773f8cfb031e4449f0e
Robert Nilsson <krnilsson@yandex.com>
2024-03-10 19:11:41 +0000

    Add some Javascript functions to call when buttons are clicked

--- assets/js/script.js
+++ assets/js/script.js
@@ -11,3 +11,22 @@ Paper disproves Spock
 Spock vaporizes Rock
 (and as it always has) Rock crushes Scissors
  */
+
+document.addEventListener("DOMContentLoaded", function() {
+    let buttons = document.getElementsByClassName("card");
+    console.log("added event listener")
+    for (let button of buttons) {
+        button.addEventListener("click", function() {
+            selectButton(this);
+        });
+    }
+});
+
+
+function selectButton(buttonName) {
+    console.log("selected card:", buttonName);
+}
+
+function playCard(){
+    console.log("play card");
+}
