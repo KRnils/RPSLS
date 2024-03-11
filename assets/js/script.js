dd999c03183e7debffe53a2c553beb2b451f87de
Robert Nilsson <krnilsson@yandex.com>
2024-03-11 22:13:44 +0000

    Make buttons animate neatly when clicked to show which player action is selected

--- assets/js/script.js
+++ assets/js/script.js
@@ -17,16 +17,37 @@ document.addEventListener("DOMContentLoaded", function() {
     console.log("added event listener")
     for (let button of buttons) {
         button.addEventListener("click", function() {
-            selectButton(this);
+            clickButton(this, buttons);
         });
     }
 });
 
 
-function selectButton(buttonName) {
-    console.log("selected card:", buttonName);
+function clickButton(currentButton, allButtons) {
+    console.log("selected card:", currentButton);
+    for (let button of allButtons){
+        if(button === currentButton){
+            button.style.transform = "translateY(-30%)";
+        } else {
+            button.style.transform = "translateY(0px)";
+        }
+    }
 }
 
 function playCard(){
     console.log("play card");
 }
+
+function animateButton(currentButton) {
+    let buttons = document.getElementsByClassName("card");
+    for (let item in buttons){
+        if(item === currentButton){
+            currentButton.style.transition = "all 500ms ease-in-out";
+            currentButton.style.transform = "translateY(-30%)";
+        } else {
+            console.log(item);
+            item.style.transform = "translateY(0px)";
+        }
+    }
+    
+  }
