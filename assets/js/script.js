e9a509207d4289dcb5fe0321cb26a1d8ae960d1b
Robert Nilsson <krnilsson@yandex.com>
2024-03-23 11:15:09 +0100

    Make slight corrections to styles to fit player actions and computer character better on all screens

--- assets/js/script.js
+++ assets/js/script.js
@@ -12,21 +12,22 @@ Spock vaporizes Rock
 (and as it always has) Rock crushes Scissors
  */
 
-document.addEventListener("DOMContentLoaded", function() {
+/* Event listener for all player cards */
+document.addEventListener("DOMContentLoaded", function () {
     let buttons = document.getElementsByClassName("card");
     console.log("added event listener")
     for (let button of buttons) {
-        button.addEventListener("click", function() {
+        button.addEventListener("click", function () {
             clickButton(this, buttons);
         });
     }
 });
 
-
+/** Function to select and animate player cards */
 function clickButton(currentButton, allButtons) {
     console.log("selected card:", currentButton);
-    for (let button of allButtons){
-        if(button === currentButton){
+    for (let button of allButtons) {
+        if (button === currentButton) {
             button.style.transform = "translateY(-30%)";
         } else {
             button.style.transform = "translateY(0px)";
@@ -34,16 +35,16 @@ function clickButton(currentButton, allButtons) {
     }
 }
 
-function playCard(){
+/** Called when player has decided what card to play, main game logic is executed here */
+function playCard() {
     console.log("play card");
 
-    //display message in game area
 }
 
 function animateButton(currentButton) {
     let buttons = document.getElementsByClassName("card");
-    for (let item in buttons){
-        if(item === currentButton){
+    for (let item in buttons) {
+        if (item === currentButton) {
             currentButton.style.transition = "all 500ms ease-in-out";
             currentButton.style.transform = "translateY(-30%)";
         } else {
@@ -51,5 +52,4 @@ function animateButton(currentButton) {
             item.style.transform = "translateY(0px)";
         }
     }
-    
-  }
+}
