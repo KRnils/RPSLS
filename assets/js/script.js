b0f3d86d85b440f2bee71ca87e40ff4a230c015c
Robert Nilsson <krnilsson@yandex.com>
2024-03-24 00:29:36 +0100

    Add game logic for selecting a player action

--- assets/js/script.js
+++ assets/js/script.js
@@ -12,44 +12,67 @@ Spock vaporizes Rock
 (and as it always has) Rock crushes Scissors
  */
 
-/* Event listener for all player cards */
+/** Add event listener for things to do when document is loaded */
 document.addEventListener("DOMContentLoaded", function () {
     let buttons = document.getElementsByClassName("card");
-    console.log("added event listener")
+    let playButton = document.getElementById("play-card-button");
+
+    // Make all card buttons have the checked attribute set to false
+    for (let button of buttons) {
+        button.checked = false;
+    }
+    
+    /* Add event listeners so all player cards call the same function */
     for (let button of buttons) {
         button.addEventListener("click", function () {
             clickButton(this, buttons);
         });
     }
+    /* Add event listener to play card button */
+    playButton.addEventListener("click", function() {
+        playCard(buttons);
+    });
+
 });
 
 /** Function to select and animate player cards */
 function clickButton(currentButton, allButtons) {
-    console.log("selected card:", currentButton);
     for (let button of allButtons) {
-        if (button === currentButton) {
+        if (button === currentButton && currentButton.checked === false) {
             button.style.transform = "translateY(-30%)";
+            button.checked=true;
         } else {
             button.style.transform = "translateY(0px)";
+            button.checked=false;
         }
     }
 }
 
 /** Called when player has decided what card to play, main game logic is executed here */
-function playCard() {
-    console.log("play card");
-
-}
-
-function animateButton(currentButton) {
-    let buttons = document.getElementsByClassName("card");
-    for (let item in buttons) {
-        if (item === currentButton) {
-            currentButton.style.transition = "all 500ms ease-in-out";
-            currentButton.style.transform = "translateY(-30%)";
-        } else {
-            console.log(item);
-            item.style.transform = "translateY(0px)";
+function playCard(playerHand) {
+    let status = document.getElementById("game-status");
+    for (let card of playerHand) {
+        if(card.checked==true){
+            let regex = new RegExp(/(?<=br>).*/);
+            let action = card.innerHTML.match(regex)[0];
+            console.log(action);
+            switch(action) {
+                case 'Rock':
+                    status.innerHTML = "Rock";
+                    break;
+                case 'Paper':
+                    status.innerHTML = "Paper";
+                    break;
+                case 'Scissors':
+                    status.innerHTML = "Scissors";
+                    break;
+                case 'Lizard':
+                    status.innerHTML = "Lizard";
+                    break;
+                case 'Spock':
+                    status.innerHTML = "Spock";
+                    break;
+            }
         }
     }
 }
